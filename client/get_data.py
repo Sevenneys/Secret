440a98fad8f1603d833dc71317f2f889968c69c0
Sevenneys <Sevenneys@gmail.com>
2026-02-08 18:07:33 +0300

    Data collection start

---
+++ client/get_data.py
@@ -0,0 +1,55 @@
+from lib import os, sys, time, platform
+from json_coding import *
+
+class CreateDataCollection:
+
+    user_id = "Null"
+    user_name = "Null"
+    oc_name = "Null"
+    
+    def data_intilization(self) -> dict:
+
+        try:
+
+            user_id = os.getpid()
+            user_name = os.getlogin()
+            oc_name = platform.platform()
+
+            coding_str = str(oc_name).lower()
+            
+            oc = ""
+
+            if sys.platform.startswith('linux'):
+                oc = "linux"
+            elif sys.platform.startswith("windows"):
+                oc = "windows"
+
+            return {
+                "user_id": user_id,
+                "user_name": user_name,
+                "oc_name": oc
+            }
+
+        except Exception as err:
+            print(f"Error: {err}")
+    
+    def write_file(self, data):
+
+        try:
+
+            desc = os.path.dirname(os.path.dirname(__file__))
+            create_descriptor = os.path.join(desc, "config", "files", "user_data.json")
+
+            with open(create_descriptor, "w+") as w_file:
+
+                js_coding = run_json(state=1, value=data)
+                w_file.write(js_coding)
+
+        except Exception as err:
+            print(f"Произошла ошибка, при записи файла: {err}")
+
+create_data = CreateDataCollection()
+
+get_data = create_data.data_intilization()
+wr_file = create_data.write_file(get_data)
+
---
+++ client/json_coding.py
@@ -0,0 +1,16 @@
+from lib import json
+
+def run_json(*, state: int, value: dict) -> str:
+    
+    if state == 1:
+        return json.dumps(value)
+    elif state == 0:
+        return json.loads(value)
+    else:
+        print(f"Неверное значение..\n\n1 - кодированние\n0 - декодированние")
+
+
+
+
+
+
