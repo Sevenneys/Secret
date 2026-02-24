e770171506491cb36c6fc0af92569fcc5b7363a9
Sevenneys <Sevenneys@gmail.com>
2026-02-24 22:13:22 +0300

    KDF is finish + hash authentication

---
+++ module/encrypt/AES/GCM/KDF/pbkdf2.py
@@ -0,0 +1,74 @@
+# Реализовать функцию по созданию хэша пароля для аутентификации (передать булевый результат).
+# Функция аутентификации всех вводных данных.
+
+# Далее идём к созданию хэша файла, куда записан хэш пароля.
+
+import hashlib
+import time
+from path import *
+
+def validation_of_password():
+
+    cir = 'йцукенгшщзхъэждлорпавыфячсмитьбюЙЦУКЕНГШЩЗХЪЭЖДЛОРПАВЫФЯЧСМИТЬБЮ'
+    list_cir = list(cir)
+
+    while True:
+
+        password_file = input(f"Задайте пароль: ")
+
+        if password_file == "":
+           print("\nОшибка при вводе пароля: [пустая строка]\n")
+           time.sleep(1)
+        else:
+            for i in password_file:
+                if i in list_cir:
+                    print("\nОшибка при вводе пароля: [кириллица - не подходящее значение]\n")
+                    break
+                else:
+                    return password_file
+
+def create_hash_password(*, absolute_path_salt: str, absolute_path_hash: str):
+
+    in_password = validation_of_password()
+
+    salt = os.urandom(16)
+    password_hash = hashlib.pbkdf2_hmac(
+        'sha256',
+        password=in_password.encode('utf-8'),
+        salt=salt,
+        iterations=30000,
+        dklen=256
+    )
+
+    try:
+        with open(absolute_path_salt, "wb+") as f_wb:
+            f_wb.write(salt)
+        with open(absolute_path_hash, "wb+") as f_wb:
+            f_wb.write(password_hash)
+    except Exception as er:
+        print("При записи файлов - произошла ошибка: {er}")
+                    
+    return
+     
+def create_verify_hash_password(set_salt: bytes, set_orig_hash: bytes, set_password_confirm: str) -> bool:
+
+    password_hash = hashlib.pbkdf2_hmac(
+        'sha256',
+        password=set_password_confirm.encode('utf-8'),
+        salt=set_salt,
+        iterations=30000,
+        dklen=256
+    )
+
+    return password_hash == set_orig_hash
+
+# set_password = 'qwerty12345'
+# confirm_password = 'qwerty12345'
+
+# salt, hash_password = create_hash_password(set_password)
+# is_verify_hash = create_verify_hash_password(salt, hash_password, confirm_password)
+
+# if is_verify_hash:
+#     print("Пароли совпадают!")
+# else:
+#     print("Пароли не совпадают!")
--- module/encrypt/AES/GCM/decr.py
+++ module/encrypt/AES/GCM/decr.py
@@ -1,6 +1,7 @@
 from lib import *
 from path import *
 import shutil
+from KDF.pbkdf2 import create_verify_hash_password
 
 def decrypt_gcm():
 
@@ -15,37 +16,57 @@ def decrypt_gcm():
 
     if os.path.isdir(create_full_path):
 
-        get_path_key = os.path.join(create_full_path, 'aes_key.bin')
-        
-        try:
-            with open(get_path_key, 'rb') as f_rb:
-                set_key = f_rb.read()
+        confirm_password = input(f"Пароль для аутентификации: ")
 
-            with open(encrypt_file, 'rb') as f_rb:
-                get_full_data = f_rb.read()
-                nonce = get_full_data[:12]
-                encrypt_text = get_full_data[12:]
+        get_salt = os.path.join(create_full_path, 'salt_pass.bin')
+        get_origin_pass_hash = os.path.join(create_full_path, 'hash_pass.bin')
+        get_path_key = os.path.join(create_full_path, 'aes_key.bin')
+
+        try:
+            with open(get_salt, 'rb') as f_rb:
+                origin_pass_salt = f_rb.read()
+            with open(get_origin_pass_hash, 'rb') as f_rb:
+                origin_pass_hash = f_rb.read()
+
+            result_in_hash = create_verify_hash_password(set_salt=origin_pass_salt, set_orig_hash=origin_pass_hash, set_password_confirm=confirm_password)
+            if result_in_hash:
+
+                try:
+                    with open(get_path_key, 'rb') as f_rb:
+                        set_key = f_rb.read()
+
+                    with open(encrypt_file, 'rb') as f_rb:
+                        get_full_data = f_rb.read()
+                        nonce = get_full_data[:12]
+                        encrypt_text = get_full_data[12:]
+
+                except Exception as er:
+                    print(f"Ошибка - файл {encrypt_file} не найден..")
+
+                aescgm = AESGCM(set_key)
+
+                try:
+                    plaintext = aescgm.decrypt(nonce, encrypt_text, None)
+
+                    with open(encrypt_file, "wb") as f_wb:
+                        f_wb.write(plaintext)
+
+                    shutil.rmtree(create_full_path)
+
+                    print("Файл успешно дешифрован!")
+
+                except InvalidTag as er:
+                    print(f"При дешифровании файла произошла ошибка: {er}")
+
+            else:
+                print(f"Ошибка - неверный пароль...")
 
         except Exception as er:
-            print(f"Ошибка - файл {encrypt_file} не найден..")
-
-        aescgm = AESGCM(set_key)
-
-        try:
-            plaintext = aescgm.decrypt(nonce, encrypt_text, None)
-
-            with open(encrypt_file, "wb") as f_wb:
-                f_wb.write(plaintext)
-
-            shutil.rmtree(create_full_path)
-
-            print("Файл успешно дешифрован!")
-
-        except InvalidTag as er:
-            print(f"При дешифровании файла произошла ошибка: {er}")
+                print(f"При получении исходного хэша, произошла ошибка: {er}")
 
     else:
         print(f"Директория: {create_full_path} - не существует!")
+        
 
 decrypt_gcm()
 
