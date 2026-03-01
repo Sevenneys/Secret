ecddd87ada1d0e3cc38110b7aaf1c01a4834e827
Sevenneys <Sevenneys@gmail.com>
2026-03-01 14:02:04 +0300

    refactoring interface.py + v2

--- client/ui/interface.py
+++ client/ui/interface.py
@@ -10,22 +10,39 @@ class MainConsoleInterface:
         self._version = version
         self.f = pyfiglet.figlet_format("SECRET", font='slant')
 
-        self.info_text = [
-            '0 - [завершить программу]', 
-            '1 - [шифрование/дешифрование]', 
-            '2 - [кодирование/декодирование]',
-            ]
+        self.info_data = {
+            'info':['0 - [завершить программу]', '1 - [шифрование/дешифрование]', '2 - [кодирование/декодирование]',],
+            'mode': ['0', '1', '2']
+        }
 
     def create_main_menu(self):
-
         print(f"{self.f}\n                                {self._version}")
-        print("-------------------------------------\n")
 
-        for element in self.info_text:
-            print(element)
-        print(f"\n-------------------------------------\n")
+        while True:
+            try:
+                print("-------------------------------------\n")
 
-        return input(f"{os.getlogin()}: ")
+                for element in self.info_data.get('info'):
+                    print(element)
+
+                print(f"\n-------------------------------------\n")
+
+                user = input(f"{os.getlogin()}: ")
+
+                if user not in self.info_data.get('mode'):
+                    print(f"ERROR: неверная комманда...\n")
+                    time.sleep(0.5)
+                else:
+                    return user
+                
+            except KeyboardInterrupt:
+                print(f"\n\nINFO: сочетание клавиш CTRL + C\nDEBUG: завершаем программу..\n")
+                time.sleep(0.5)
+                sys.exit()
+            except Exception as er:
+                print(f"ERROR: неожиданное исключение {er}")
+
+        
 
 
 
